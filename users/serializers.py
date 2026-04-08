79547f7c0b0b46348fbdd0df39586710a6e145f5
RyuHawon <hfixjsj49@gmail.com>
2026-04-08 16:31:44 +0900

    commit

---
+++ users/serializers.py
@@ -0,0 +1,14 @@
+from rest_framework import serializers
+from .models import User
+
+
+class SignupSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ["email", "username", "password", "phone"]
+
+    def create(self, validated_data):
+        password = validated_data.pop("password")
+        return User.objects.create_user(password=password, **validated_data)
