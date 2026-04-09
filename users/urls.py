d6a543c8e4463689ee9b03d469a66815f409b381
RyuHawon <hfixjsj49@gmail.com>
2026-04-09 16:53:46 +0900

    refactor(users): 모델/시리얼라이저만 남기고 정리
    - views, services, urls 초기화
    - 기능을 하나씩 직접 구현하기 위한 준비

--- users/urls.py
+++ users/urls.py
@@ -1,11 +1,1 @@
-from django.urls import path
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-
-from .views import LogoutView, SignupView
-
-urlpatterns = [
-    path("signup/", SignupView.as_view(), name="signup"),
-    path("login/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
-    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
-    path("logout/", LogoutView.as_view(), name="logout"),
-]
+urlpatterns = []
